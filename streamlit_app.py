042b7e4e926eff12ffa4efcfd7a5504cf2660e0e
tx-smitht <112910116+tx-smitht@users.noreply.github.com>
2023-08-18 13:14:56 -0400

    Update streamlit_app.py
    remove raw and sep name in uri

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Normalize the json using pandas
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
