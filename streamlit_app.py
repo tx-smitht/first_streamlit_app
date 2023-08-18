9d6bcc56f2d233eab485ea59bd6ac94c9c5c6333
tx-smitht <112910116+tx-smitht@users.noreply.github.com>
2023-08-18 13:16:31 -0400

    Update streamlit_app.py
    adding fruit entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,11 @@ st.dataframe(fruits_to_show)
 # new section
 st.header("Fruityvice Fruit Advice!")
 
+fruit_choice = st.text_input('What fruit would you like info about?', 'Kiwi')
+st.write(f'the User entered {fruit_choice}')
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the json using pandas
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
