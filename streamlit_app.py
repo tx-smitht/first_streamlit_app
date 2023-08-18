c230d9bbfcc573678042b12651d5056e4070280e
tx-smitht <112910116+tx-smitht@users.noreply.github.com>
2023-08-18 13:12:19 -0400

    Update streamlit_app.py
    add requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,8 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
+
+# new section
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
