3311984f8d4e6633fdc9e8445922d67ef17aaf74
tx-smitht <112910116+tx-smitht@users.noreply.github.com>
2023-08-18 13:12:56 -0400

    Update streamlit_app.py
    parse response and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 # new section
+st.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
