3216accb0dccfb031ecf9efe06f6122cbc33b2d8
tx-smitht <112910116+tx-smitht@users.noreply.github.com>
2023-08-18 11:38:28 -0400

    Update streamlit_app.py
    make df dynamic off multi-select

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.text('🥑🍞Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
