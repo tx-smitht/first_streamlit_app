c31bcdb549975db356c2a414c5e3018c63eb1500
tx-smitht <112910116+tx-smitht@users.noreply.github.com>
2023-08-18 11:30:21 -0400

    Update streamlit_app.py
    added emojis and toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title('My Parents\' Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥑🍞Avocado Toast')
